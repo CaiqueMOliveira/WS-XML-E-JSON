8f246d1119974b3597c12714ee82154c2c97a5df
CAIQUE MEIRA OLIVEIRA <16254840@CFP127ALUNO.LOCAL>
2017-11-16 09:49:26 -0200

    first commit

---
+++ contatos_JSON.php
@@ -0,0 +1,144 @@
+<?php
+$nome="";
+$telefone="";
+$celular="";
+$email="";
+$obs="";
+$botao="Inserir";
+
+//programação para inserir no xml
+if (isset($_GET["btnsalvar"]))
+{  
+	//variaveis que vieram via 
+	//GET no botao Salvar
+	$nome=$_GET["txtnome"];
+	$telefone=$_GET["txttelefone"];
+	$celular=$_GET["txtcelular"];
+	$email=$_GET["txtemail"];
+	$obs=$_GET["txtobs"];
+    
+    // NOME DO ARQUIVO SERA CRIADO
+    $nomeArquivo = "dados.json";
+    
+    // CRIA OU ABRE O ARQUIVO CASO JA EXISTA 
+    $arquivo = fopen($nomeArquivo, "w");
+    
+    // CRIANDO UM array DE DADOS QUE SERA ARMAZENADO NO ARQUIVO JSON
+    $arrayDados = array(
+    
+        "nome"=>$nome,
+        "telefone"=>$telefone,
+        "celular"=>$celular,
+        "email"=>$email,
+        "obs"=>$obs   
+    );
+    
+    // PARA VISUALIZAR O CONTUDO DE UM OBJET,  UTILIZE O print_r OU var_dump (var_dump EXIBI MAIS DETALHES)
+    // print_r($arrayDados);
+    // var_dump($arrayDados);
+    
+    // GRAVANDO OS DADOS DO ARRAY EM FORMATO JSON NO ARQUIVO
+    fwrite($arquivo, json_encode($arrayDados));
+    
+    //FECHANDO O ARQUIVO
+    fclose($arquivo);
+}	
+
+?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+<link rel="stylesheet" type="text/css" href="css/style.css">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+</head>
+
+<body>
+
+<div id="principal">
+	
+    
+     <div id="conteudo">
+    	<div id="cadastro">
+        	
+            <form name="frmcontatos" method="get" action="contatos_JSON.php">
+            
+                <table id="tblcadastro">
+                  <tr>
+                    <td colspan="2">Gerando XML</td>
+                  </tr>
+                  <tr>
+                    <td>Nome:</td>
+                    <td><input class="caixa" name="txtnome" type="text"   value="<?php echo($nome) ?>" required /></td>
+                  </tr>
+                  <tr>
+                    <td>Telefone:</td>
+                    <td><input class="caixa" name="txttelefone" type="text"  value="<?php echo($telefone) ?>" /></td>
+                  </tr>
+                  <tr>
+                    <td>Celular:</td>
+                    <td><input class="caixa" name="txtcelular" type="text" value="<?php echo($celular) ?>" /></td>
+                  </tr>
+                  <tr>
+                    <td>Email:</td>
+                    <td><input class="caixa" name="txtemail" type="text" value="<?php echo($email) ?>" required /></td>
+                  </tr>
+                  <tr>
+                    <td>Obs:</td>
+                    <td><textarea name="txtobs" cols="20" rows="5"><?php echo($obs) ?></textarea></td>
+                  </tr>
+                  <tr>
+                    <td><input name="btnsalvar" type="submit" value="<?php echo($botao) ?>" /></td>
+                    <td><input name="btnlimpar" type="reset" value="Limpar" /></td>
+                  </tr>
+                </table>
+            
+            </form>
+
+        </div>
+        <div id="consulta">
+        	<table id="tblconsulta">
+              <tr>
+                <td colspan="5">Consulta XML</td>
+              </tr>
+              <tr>
+                <td>Nome</td>
+                <td>Telefone</td>
+                <td>Celular</td>
+                <td>Email</td>
+				<td>Obs</td>
+               
+              </tr>
+			  <?php 
+				
+			  ?>
+				  <tr>
+					<td></td>
+					<td></td>
+					<td></td>
+					<td></td>
+					<td></td>
+					
+				  </tr>
+            <?php 
+				
+				
+			
+			?>
+            </table>
+
+        </div>
+           
+    </div>
+    
+
+    
+</div>
+
+</body>
+</html>
+
+
+
---
+++ contatos_XML.php
@@ -0,0 +1,176 @@
+<?php
+$nome="";
+$telefone="";
+$celular="";
+$email="";
+$obs="";
+$botao="Inserir";
+
+//programação para inserir no xml
+if (isset($_GET["btnsalvar"]))
+{  
+	//variaveis que vieram via 
+	//GET no botao Salvar
+	$nome=$_GET["txtnome"];
+	$telefone=$_GET["txttelefone"];
+	$celular=$_GET["txtcelular"];
+	$email=$_GET["txtemail"];
+	$obs=$_GET["txtobs"];
+    
+    // NOME DO ARQUIVO A SER GERADO EM XML
+    $nomeArquivo = "dados.xml";
+    
+    // ABRE OU CRIA O ARQUIVO CAJO AINDA NAO EXISTA E DEFINE O MODO DE OPERCAO (w = write ou r = read) 
+    $arquivo = fopen($nomeArquivo,"w");
+    
+    /*
+    ESCREVE A PRIMEIRA LINHA DO ARQUIVO XML, QUE E A REFERENCIA DO PADRAO DE MANIPULACAO DE DADOS QUE SERA REALIZADO NA ESCRITA DO ARQUIVO.
+    OBS.: ESTA LINHA E OBRIGATORIA.
+    */
+    fwrite($arquivo, "<?xml version='1.0'  encoding='ISO-8859-1'?>\r\n");
+    
+    // CRIAMOS UM NODE PRINCIPAL DO ARQUIVO XML, O NOME DESTE NODE PODE SER CRIADO CONFORME A NECESSIDADE DAS REFERENCIAS 
+    fwrite($arquivo, "<contatos>\r\n");
+    
+        /* 
+        CRIA UMA INDENTIFICACAO DO ITEM NO ARQUIVO XML.
+        SE ESTIVESSEMOS FAZENDO UM SELECT NO DB O ATRIBUTO ID SERIA A CHAVE PRIMARIA,
+        LEMBRE-SE QUE ISTO TUDO SERIA UMA REPETICAO DE DADOS DO BANCO.
+        */  
+        fwrite($arquivo,"<registro id='1'>\r\n");
+    
+        
+        $dadosConteudo = 
+            
+            /*
+            CRIAMOS TODOS OS NODES DOS DADOS QUE SERAO ESCRINOS NO XML
+            OBS.: LEBRANDO QUE PARA CADA DADO NEVE-SE CRIAR OS EU NODE.
+            */
+            "<nome>".$nome."</nome>\r\n
+            <telefone>".$telefone."</telefone>\r\n
+            <celular>".$celular."</celular>\r\n
+            <email>".$email."</email>\r\n
+            <obs>".$obs."</obs>\r\n";
+    
+    
+        // ESCREVE TODOS OS ITENS NO ARQUIUVO XML
+        fwrite($arquivo, $dadosConteudo);
+    
+        // FECHA O NODE REGISTRO
+        fwrite($arquivo,"</registro>\r\n");
+    
+    // FECHA O NODE PRINCIPAL (CONTATO)
+    fwrite($arquivo,"</contatos>\r\n");
+    
+    // FECHA O OBJETO QUE REPRESENTA O ARQUIVO
+    fclose($arquivo);
+
+}	
+
+?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+<link rel="stylesheet" type="text/css" href="css/style.css">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+</head>
+
+<body>
+
+<div id="principal">
+	
+    
+     <div id="conteudo">
+    	<div id="cadastro">
+        	
+            <form name="frmcontatos" method="get" action="contatos_XML.php">
+            
+                <table id="tblcadastro">
+                  <tr>
+                    <td colspan="2">Gerando XML</td>
+                  </tr>
+                  <tr>
+                    <td>Nome:</td>
+                    <td><input class="caixa" name="txtnome" type="text"   value="<?php echo($nome) ?>" required /></td>
+                  </tr>
+                  <tr>
+                    <td>Telefone:</td>
+                    <td><input class="caixa" name="txttelefone" type="text"  value="<?php echo($telefone) ?>" /></td>
+                  </tr>
+                  <tr>
+                    <td>Celular:</td>
+                    <td><input class="caixa" name="txtcelular" type="text" value="<?php echo($celular) ?>" /></td>
+                  </tr>
+                  <tr>
+                    <td>Email:</td>
+                    <td><input class="caixa" name="txtemail" type="text" value="<?php echo($email) ?>" required /></td>
+                  </tr>
+                  <tr>
+                    <td>Obs:</td>
+                    <td><textarea name="txtobs" cols="20" rows="5"><?php echo($obs) ?></textarea></td>
+                  </tr>
+                  <tr>
+                    <td><input name="btnsalvar" type="submit" value="<?php echo($botao) ?>" /></td>
+                    <td><input name="btnlimpar" type="reset" value="Limpar" /></td>
+                  </tr>
+                </table>
+            
+            </form>
+
+        </div>
+        <div id="consulta">
+        	<table id="tblconsulta">
+              <tr>
+                <td colspan="5">Consulta XML</td>
+              </tr>
+              <tr>
+                <td>Nome</td>
+                <td>Telefone</td>
+                <td>Celular</td>
+                <td>Email</td>
+				<td>Obs</td>
+               
+              </tr>
+			  <?php 
+                
+                /*
+                PERMITE ABRIR UM ARQUIVO XML PARA LEITURA DE DADOS
+                PRECISAMOS PASSAR O CAMINHO COMPLETO ATE O ARQUIVO XML, CASO ESTEJA ON WEB NECESSITA-SE PASSAR O ENDERECO ATE O CAMINHO XML (www.site./arquivo.xml)  
+                RETURN = array;
+                */
+//				$arquivoXML = simplexml_load_file("dados.xml");
+				$arquivoXML = simplexml_load_file("dados.xml");
+                
+                // CONTA QUANTOS INDICES EXISTEM EM UM array DE DADOS
+                $qtdeXML = count($arquivoXML);
+                               
+                for($i = 0; $i < $qtdeXML; $i++){
+			  ?>
+				  <tr>
+					<td><?php echo($arquivoXML->registro[$i]->nome); ?></td>
+					<td><?php echo($arquivoXML->registro[$i]->telefone); ?></td>
+					<td><?php echo($arquivoXML->registro[$i]->celular); ?></td>
+					<td><?php echo($arquivoXML->registro[$i]->email); ?></td>
+					<td><?php echo($arquivoXML->registro[$i]->obs); ?></td>
+				  </tr>
+            <?php 
+                }			
+			?>
+            </table>
+
+        </div>
+           
+    </div>
+    
+
+    
+</div>
+
+</body>
+</html>
+
+
+
